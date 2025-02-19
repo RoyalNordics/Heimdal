1f6e56a09ea4514f0f94c9edd1e3c1401f904fcd
Thomas Flyttov Møller <thomas.flyttov@hotmail.com>
2025-02-19 09:46:39 +0000

    Fix: Ensure Gunicorn starts Flask correctly

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# Start Flask-serveren med Gunicorn
-exec gunicorn -b 0.0.0.0:$PORT middleware:app
+# Start Flask-serveren med Gunicorn (hvis middleware.py ligger i src/)
+exec gunicorn --chdir src -b 0.0.0.0:$PORT middleware:app
